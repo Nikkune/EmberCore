aab0fee7e39b115c97991bd699bcea4487f87522
Nikkune <dev.2tvhg@passmail.net>
2026-03-28 15:38:53 +0100

    feat: ✨ Add Prettier with configuration, eslint integration, and ignore files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,29 +1,49 @@
-import tseslint from "@typescript-eslint/eslint-plugin";
-import parser from "@typescript-eslint/parser";
+import tseslint from '@typescript-eslint/eslint-plugin';
+import parser from '@typescript-eslint/parser';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default [
 	{
-		files: ["**/*.ts"],
+		ignores: ['luas/**', 'build-scripts/**', 'manifests/**', 'node_modules/**'],
+	},
+	{
+		files: ['**/*.ts'],
 		languageOptions: {
 			parser,
 			parserOptions: {
-				sourceType: "module",
-				ecmaVersion: "latest",
+				sourceType: 'module',
+				ecmaVersion: 'latest',
 			},
 		},
 		plugins: {
-			"@typescript-eslint": tseslint,
+			'@typescript-eslint': tseslint,
 		},
 		rules: {
-			"@typescript-eslint/consistent-type-imports": [
-				"error",
+			'@typescript-eslint/consistent-type-imports': [
+				'error',
 				{
-					prefer: "type-imports",
-					fixStyle: "separate-type-imports",
+					prefer: 'type-imports',
+					fixStyle: 'separate-type-imports',
 				},
 			],
-
-			"@typescript-eslint/no-unused-vars": "warn",
+			'@typescript-eslint/no-unused-vars': [
+				'warn',
+				{
+					argsIgnorePattern: '^_',
+					varsIgnorePattern: '^_',
+					caughtErrorsIgnorePattern: '^_',
+				},
+			],
+			'max-len': [
+				'error',
+				{
+					code: 120,
+					ignoreImports: true,
+					ignoreStrings: true,
+					ignoreTemplateLiterals: true,
+				},
+			],
 		},
 	},
-]
+	eslintConfigPrettier,
+];
